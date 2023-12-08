4d8883a3dc06386b2aecfdd3a8b3e8d7066ec565
Pradeep Kumar <61448614+pradytpk@users.noreply.github.com>
2023-12-09 07:39:46 +0900

    array dsa

--- LDSA/src/arraydsa/Main.java
+++ LDSA/src/arraydsa/Main.java
@@ -6,11 +6,13 @@ public class Main {
 		arr.insert(1);
 		arr.insert(2);
 		arr.insert(3);
-		arr.insert(4);
+		arr.insert(400);
 		arr.insert(5);
-		
-		System.out.println(arr.indexOf(5));
-		System.out.println(arr.indexOf(2));
+		arr.insert(5);
+		arr.insert(3);
+		System.out.println(arr);
+		arr.reverse();
+		System.out.println(arr);
 
 	}
 }
--- LDSA/src/arraydsa/MyArray.java
+++ LDSA/src/arraydsa/MyArray.java
@@ -10,11 +10,20 @@ public class MyArray {
 		this.currentIndex = 0;
 	}
 
+	// Insert the array
 	public void insert(int value) {
+		if (currentIndex == items.length) {
+			int[] temp = new int[items.length * 2];
+			for (int i = 0; i < currentIndex; i++) {
+				temp[i] = items[i];
+			}
+			items = temp;
+		}
 		this.items[currentIndex] = value;
 		currentIndex++;
 	}
 
+	// Searching the array list
 	public int indexOf(int value) {
 		// Linear search
 		for (int i = 0; i < this.currentIndex; i++) {
@@ -25,14 +34,67 @@ public class MyArray {
 		return -1;
 	}
 
+	// Remove the array
+	public void removeAt(int index) {
+		if (index >= this.currentIndex)
+			throw new IllegalArgumentException();
+		for (int i = index; i <= this.currentIndex - 2; i++) {
+			this.items[i] = this.items[i + 1];
+		}
+		currentIndex--;
+		this.items[this.currentIndex] = 0;
+	}
+
+	// max of the array
+	public int max() {
+		int result = this.items[0];
+		for (int i = 0; i < this.currentIndex; i++) {
+			if (items[i] > result) {
+				result = items[i];
+			}
+		}
+		return result;
+	}
+
+	// minimum of the array
+	public int min() {
+		int result = this.items[0];
+		for (int i = 0; i < this.currentIndex; i++) {
+			if (items[i] < result) {
+				result = items[i];
+			}
+		}
+		return result;
+	}
+
+	// Reverse Array
+
+	public void reverse() {
+		int i = 0;
+		int j = this.currentIndex - 1;
+		while (i < j) {
+			int temp = items[i];
+			items[i] = items[j];
+			items[j] = temp;
+			i++;
+			j--;
+		}
+
+	}
+
+	// Print the array
 	@Override
 	public String toString() {
 		StringBuilder str = new StringBuilder();
 		str.append("[");
-		for (int i : items) {
-			str.append(i).append(", ");
+		for (int i = 0; i < this.currentIndex; i++) {
+			str.append(this.items[i]);
+			if (i < this.currentIndex - 1) {
+				str.append(", ");
+			}
 		}
-		str.append("\b").append("\b").append("]");
+		str.append("]");
 		return str.toString();
 	}
+
 }
